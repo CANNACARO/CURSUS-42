6ac0627dfc34192b63d86345112143047ac26172
CANNACARO <al398457@uji.es>
2024-04-17 20:00:57 +0200

    hasta ft_strlcpy

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -3,11 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcaro-lo <jcaro-lo@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: jcaro-lo <jcaro-lo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 17:31:43 by jcaro-lo          #+#    #+#             */
-/*   Updated: 2024/04/14 17:31:45 by jcaro-lo         ###   ########.fr       */
+/*   Updated: 2024/04/17 20:00:36 by jcaro-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	src_size;
+	size_t	i;
+
+	src_size = 0;
+	i = 0;
+	while (src[src_size])
+		src_size++;
+	while (i < size -1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (size > 0)
+		dst[i] = '\0';
+	return (src_size);
+}
+
+/*int	main(void)
+{
+	char src[]="hola";
+	char dst[]= "adios";
+	size_t	x;
+
+	x = ft_strlcpy(dst, src, 0);
+	printf("%s   %ld", dst, x);
+}*/
--- Libft/prueba.c
+++ Libft/prueba.c
@@ -6,23 +6,30 @@
 /*   By: jcaro-lo <jcaro-lo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 16:54:53 by jcaro-lo          #+#    #+#             */
-/*   Updated: 2024/04/16 20:30:22 by jcaro-lo         ###   ########.fr       */
+/*   Updated: 2024/04/17 20:00:46 by jcaro-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
+#include <bsd/string.h>
 
 
 int	main(void)
 {
 	char src[]= "hola";
 	char dest[]= "adiossss";
+	int x;
 	
-	memmove(dest, src, 6);
-	printf("%s",dest);
+	x = strlcpy(dest, src, 0);
+	printf("%s   %d",dest, x);
 }
 
-//memmove original me lanza un error al poner el numero de caracteres mayor al de la fuente
-//prueba.c:22:9: error: ‘__builtin_memcpy’ reading 6 bytes from a region of size 5 [-Werror=stringop-overread]
-//   22 |         memmove(dest, src, 6);
+/*memmove original me lanza un error al poner el numero de caracteres mayor al de la fuente
+prueba.c:22:9: error: ‘__builtin_memcpy’ reading 6 bytes from a region of size 5 [-Werror=stringop-overread]
+   22 |         memmove(dest, src, 6);
+   
+   Mi ft_strlcpy hace segfault (core dumped) cuando pon size = 0
+   
+   
+   */
