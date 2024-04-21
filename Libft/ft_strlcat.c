e151aa3f81c629d92924ff1a2677b12d99df314c
CANNACARO <al398457@uji.es>
2024-04-21 08:34:27 +0200

    listo

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcaro-lo <jcaro-lo@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: jcaro-lo <jcaro-lo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 17:32:14 by jcaro-lo          #+#    #+#             */
-/*   Updated: 2024/04/14 17:32:18 by jcaro-lo         ###   ########.fr       */
+/*   Updated: 2024/04/21 07:59:32 by jcaro-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
 	i = 0;
-	if (dstsize <= dst_len)
+	/*if (dstsize <= dst_len)
 	{
 		dst_len = dstsize;
 		return (dst_len + src_len);
-	}
+	}*/
 	while (src[i] && dst_len + i < dstsize -1)
 	{
 		dst[dst_len + i] = src[i];
