1e211254715b451640cbcc868f97ec4ec9a26fc8
Jose Ismael Caro Lopez <jcaro-lo@c3r4s3.42malaga.com>
2024-05-11 14:31:20 +0200

    the fucking spliiitt

--- Libft/prueba.c
+++ Libft/prueba.c
@@ -6,7 +6,7 @@
 /*   By: jcaro-lo <jcaro-lo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 16:54:53 by jcaro-lo          #+#    #+#             */
-/*   Updated: 2024/05/05 17:50:35 by jcaro-lo         ###   ########.fr       */
+/*   Updated: 2024/05/11 13:13:13 by jcaro-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,41 @@
 #include <stdlib.h>
 #include "libft.h"
 
+int main()
+{
+
+	char s[] = "chatarra";
+	int i = 0;
+	char c = 'a';
+
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			while (s[i] != c || s[i] != '\0')
+				i++;
+		}
+		else
+			i++;
+	}
+}
+
 /*int main() {//main para el split
-    const char *phrase = "OpenAI is an artificial intelligence company.";
-    char **words = ft_split(phrase, 'a');
+	const char *phrase = "OpenAI is an artificial intelligence company.";
+	char **words = ft_split(phrase, 'a');
 
-    if (words) {
-        int i = 0;
-        while (words[i] != NULL) {
-            printf("Word %d: %s\n", i+1, words[i]);
-            i++;
-        }
+	if (words) {
+		int i = 0;
+		while (words[i] != NULL) {
+			printf("Word %d: %s\n", i+1, words[i]);
+			i++;
+		}
 
-        // Free the allocated memory for the words array
-        free(words);
-    } else {
-        printf("Memory allocation failed.\n");
-    }
+		// Free the allocated memory for the words array
+		free(words);
+	} else {
+		printf("Memory allocation failed.\n");
+	}
 
-    return 0;
+	return 0;
 }*/
