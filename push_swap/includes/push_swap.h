2ede5274276a41dd571f26d97e01f6273bbc63a1
CANNACARO <al398457@uji.es>
2025-03-29 10:47:41 +0100

    comienzo push swap

---
+++ push_swap/includes/push_swap.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcaro-lo <jcaro-lo@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 10:13:25 by jcaro-lo          #+#    #+#             */
+/*   Updated: 2025/03/29 10:22:21 by jcaro-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include "../libft/libft.h"
+
+typedef struct s_stack
+{
+	t_list start;
+	int size;
+}	t_stack;
+
+
+
+
+
+
+#endif
